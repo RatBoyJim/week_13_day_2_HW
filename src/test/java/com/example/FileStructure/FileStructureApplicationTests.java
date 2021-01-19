c9543ea3f1925e19c9dcff25ae6e777a676d5db1
RatBoyJim <ratboyjim@gmail.com>
2021-01-19 16:30:04 +0000

    first commit

---
+++ src/test/java/com/example/FileStructure/FileStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FileStructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
